a20dbb38733f2787cdda96cd55a689b21c8b45e9
Apelete <adzohonouapelete@gmail.com>
2022-06-29 09:50:11 +0000

    Comment php Script

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
 
 // Just an animation for the navbar animation
-
+// getting navbar element
 const navbar = document.querySelector(".menu_bar");
-
+// envent for changing the background color of the menubar
 window.addEventListener('scroll', () => {
+  // if we scroll to bottom more than 2
   if (window.scrollY > window.screen.height / 3) {
+    // make white
     navbar.classList.add('nav_bar_white');
   } else {
+    // or make transparent
     navbar.classList.remove('nav_bar_white');
   }
 });
-
+// end of the event
 // Just an animation for the navbar animation
 
 // Start ScrollTop btn
-
+// get btn element
 const scrollBtn = document.querySelector("#myBtn");
-
+// function for hide or display the button
 window.onscroll = function () {
+  // if we scroll to the bottom more than 400px
   if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    // display the button 
     scrollBtn.classList.add('myBtn_none');
   } else {
+    // else hide the button
     scrollBtn.classList.remove('myBtn_none');
   }
 }
-
+// End of the function 
+// event for scrolling to the top of the page when clicking the button
 scrollBtn.addEventListener('click', () => {
+  // scrollTo(x,y), x(horizontally) and y(vertically)
   window.scrollTo(0, 0);
 });
 // End ScrollTop btn
 
 // Img slide script
-
-
 
 // **** recuperation des images****
 let img_slider = document.getElementsByClassName('img_slider');
